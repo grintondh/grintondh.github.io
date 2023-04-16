234bb07b1d7111fdbcece51ea586444ada9a7da0
Tạ Công Nam <90957755+grintondh@users.noreply.github.com>
2023-04-16 21:29:14 +0700

    Update DataProcessing.cs
    Add some new functions

--- OOP/DataProcessing.cs
+++ OOP/DataProcessing.cs
@@ -1,12 +1,15 @@
+using Accessibility;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Text.Json.Serialization;
 using System.Windows.Forms;
 
-namespace Calculator
+namespace Learning_System.ExternalClass
 {
     public class DataProcessing
     {
@@ -15,10 +18,11 @@ namespace Calculator
         private List<Type> ShowColumnsType { get; set; } = new List<Type>();
         private static int length { get; set; }
         private static int Limit { get; set; }
-        private static List<string> Condition { get; set; }
-        private static List<string> Columns { get; set; }
+        private static List<string> Condition { get; set; } = new List<string>();
+        private static List<string> Columns { get; set; } = new List<string>();
         private static int Offset { get; set; }
-        private List<Tuple<int,int>> SelectedRow { get; set; } = new List<Tuple<int,int>>();
+        private List<Tuple<int, int>> SelectedRow { get; set; } = new List<Tuple<int, int>>();
+        public static List<string> emptyList { get; } = new List<string>();
 
         private const int DEFAULT_LIMIT = 25;
 
@@ -97,32 +101,30 @@ namespace Calculator
         /// </summary>
         /// <param name="_offset">First index you want to get</param>
         /// <param name="_limit">Number of elements you want to get</param>
-        /// <param name="_query">Queries List</param>
-        /// <param name="_columns">Columns List</param>
+        /// <param name="_query">Queries List (optional)</param>
+        /// <param name="_columns">Columns List (optional)</param>
         /// <return>Return DataTable</return>
-        public DataTable GetList(int _offset, int _limit, List<string> _query, List<string> _columns)
+        public DataTable GetList(int _offset, int _limit, List<string> _query, List<string> _columns, string _sort)
         {
             try
             {
-                if (_query != null && _query.Count % 2 != 0 && (_query.Count != 1 || _query[0] != "SAME"))
+                if (_query.Count % 2 != 0 && (_query.Count != 1 || _query[0] != "SAME"))
                     throw new Exception();
-                    
+
+                int _archievedOffset = Offset, _archievedLimit = Limit;
 
                 Offset = _offset;
                 Limit = _limit;
 
-                if (_query != null && _query.Count == 1 && _query[0] == "SAME")
+                if (_query.Count == 1 && _query[0] == "SAME")
                     Condition = Condition;
-                else 
+                else
                     Condition = _query;
 
-                if (_columns != null && _columns.Count == 1 && _columns[0] == "SAME")
-                    Columns = Columns;    
+                if (_columns.Count == 1 && _columns[0] == "SAME")
+                    Columns = Columns;
                 else
                     Columns = _columns;
-
-                if (Columns != null && (Columns.Contains("NotDelete") == false || Columns.Contains("delete") == false))
-                    throw new Exception();
 
                 /// Correction offset and limit value
                 Limit = Math.Min(Math.Max(0, Limit), length);
@@ -130,16 +132,16 @@ namespace Calculator
 
                 DataTable _dataTable = new DataTable();
 
-                if(Columns != null)
+                if (Columns.Count > 0)
                 {
                     for (int _j = 0; _j < Columns.Count; _j++)
                     {
                         string _columnName = Columns[_j];
                         bool _isExistColumn = false;
 
-                        for(int _k = 0; _k < ShowColumnsName.Count; _k++)
+                        for (int _k = 0; _k < ShowColumnsName.Count; _k++)
                         {
-                            if(_columnName == ShowColumnsName[_k])
+                            if (_columnName == ShowColumnsName[_k])
                             {
                                 Type _columnType = ShowColumnsType[_k];
                                 _dataTable.Columns.Add(_columnName, _columnType);
@@ -164,6 +166,7 @@ namespace Calculator
                 }
 
                 SelectedRow.Clear();
+
                 int _gotCount = 0;
 
                 for (int _i = Offset; _i < length && _gotCount < Limit; _i++)
@@ -172,12 +175,30 @@ namespace Calculator
 
                     bool _isSatisfy = true;
 
-                    if(Condition != null)
+                    if (Condition.Count > 0)
                     {
                         for (int _j = 0; _j < Condition.Count; _j += 2)
                         {
-                            var x = ListElements[_i][Condition[_j]].ToString();
-                            if (ListElements[_i][Condition[_j]].ToString() != Condition[_j + 1])
+                            if (Condition[_j] == "GetMaxOn")
+                                continue;
+
+                            var _elementObj = ListElements[_i][Condition[_j]];
+                            if (_elementObj == null)
+                                continue;
+
+                            string x = _elementObj.ToString();
+
+                            if (Condition[_j + 1].Length > 7 && Condition[_j + 1].Substring(0, 7) == "CONTAIN")
+                            {
+                                string _compareValue = Condition[_j + 1].Substring(7, Condition[_j + 1].Length - 7);
+
+                                if (x.Contains(_compareValue) == false)
+                                {
+                                    _isSatisfy = false;
+                                    break;
+                                }
+                            }
+                            else if (x != Condition[_j + 1])
                             {
                                 _isSatisfy = false;
                                 break;
@@ -188,10 +209,11 @@ namespace Calculator
                     if (_isSatisfy == false)
                         continue;
 
-                    SelectedRow.Add(Tuple.Create<int,int>(_i, _gotCount));
+                    SelectedRow.Add(Tuple.Create(_i, _gotCount));
+
                     _gotCount++;
 
-                    if(Columns != null)
+                    if (Columns.Count > 0)
                     {
                         for (int _j = 0; _j < Columns.Count; _j++)
                         {
@@ -213,6 +235,13 @@ namespace Calculator
 
                 Limit = _gotCount;
 
+                if(_sort != null)
+                {
+                    DataView _dv = _dataTable.DefaultView;
+                    _dv.Sort = _sort;
+                    _dataTable = _dv.ToTable();
+                }
+
                 return _dataTable;
             }
             catch (Exception ex)
@@ -223,6 +252,47 @@ namespace Calculator
                     Application.Exit();
                 return null;
             }
+        }
+
+        public DataTable GetList(int _offset, int _limit)
+        {
+            return GetList(_offset, _limit, emptyList, emptyList, null);
+        }
+
+        public DataTable GetList(int _offset, int _limit, string _sort)
+        {
+            return GetList(_offset, _limit, emptyList, emptyList, _sort);
+        }
+
+        public DataTable GetList(int _offset, int _limit, List<string> _query, string _sort)
+        {
+            return GetList(_offset, _limit, _query, emptyList, _sort);
+        }
+
+        public DataTable GetList(int _offset, int _limit, List<string> _query, List<string> _columns)
+        {
+            return GetList(_offset, _limit, _query, _columns, null);
+        }
+
+        /// <summary>
+        /// Return max / min datarow satisfy conditions
+        /// </summary>
+        /// <param name="_offset">Offset</param>
+        /// <param name="_limit">Limit</param>
+        /// <param name="_query">Conditions list</param>
+        /// <param name="_sort">Sort conditions</param>
+        /// <param name="_maxMin">MAX / MIN</param>
+        /// <returns>A datarow</returns>
+        public DataRow GetMaxMin(int _offset, int _limit, List<string> _query, string _sort, string _maxMin)
+        {
+            DataTable _sortedDB = GetList(_offset, _limit, _query, _sort);
+
+            if (_maxMin == "MAX")
+                return _sortedDB.Rows[_sortedDB.Rows.Count - 1];
+            else if (_maxMin == "MIN")
+                return _sortedDB.Rows[0];
+            else
+                return null;
         }
 
         /// <summary>
@@ -240,7 +310,7 @@ namespace Calculator
         /// <returns></returns>
         public Tuple<int, int> GetOffsetLimitNow()
         {
-            Tuple<int, int> _tuple = Tuple.Create<int, int>(Offset, Limit);
+            Tuple<int, int> _tuple = Tuple.Create(Offset, Limit);
             return _tuple;
         }
 
@@ -303,8 +373,8 @@ namespace Calculator
             {
                 if (_dataTable.Rows[_indexInTable].Field<bool>("NotDelete") == false)
                 {
-                    foreach(var _i  in SelectedRow)
-                        if(_i.Item2 == _indexInTable)
+                    foreach (var _i in SelectedRow)
+                        if (_i.Item2 == _indexInTable)
                         {
                             ListElements.RemoveAt(_i.Item1);
                             length--;
@@ -331,14 +401,14 @@ namespace Calculator
             if (_dataTable == null || _indexInTable >= _dataTable.Rows.Count)
             {
                 MessageBox.Show("Couldn't change element", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return ;
+                return;
             }
             else
             {
                 foreach (var _i in SelectedRow)
                     if (_i.Item2 == _indexInTable)
                     {
-                        if(Columns != null)
+                        if (Columns != null)
                         {
                             for (int _j = 0; _j < Columns.Count; _j++)
                             {
@@ -358,8 +428,84 @@ namespace Calculator
                         break;
                     }
 
-                return ;
+                return;
             }
+        }
+
+        /// <summary>
+        /// Change all Elements which satisfy with conditions
+        /// </summary>
+        /// <param name="_query">Queries List</param>
+        /// <param name="_newValue">New value in JObject</param>
+        /// <return>Return DataTable</return>
+        public void ChangeElementswithCondition(List<string> _query, JObject _newValue)
+        {
+            try
+            {
+                if (_query.Count % 2 != 0 && (_query.Count != 1 || _query[0] != "SAME"))
+                    throw new Exception();
+
+                DataTable _dataTable = new DataTable();
+
+                for (int _i = 0; _i < length; _i++)
+                {
+                    bool _isSatisfy = true;
+
+                    if (Condition.Count > 0)
+                    {
+                        for (int _j = 0; _j < Condition.Count; _j += 2)
+                        { 
+                            var _elementObj = ListElements[_i][Condition[_j]];
+                            if (_elementObj == null)
+                                continue;
+
+                            string x = _elementObj.ToString();
+
+                            if (Condition[_j + 1].Length > 7 && Condition[_j + 1].Substring(0, 7) == "CONTAIN")
+                            {
+                                string _compareValue = Condition[_j + 1].Substring(7, Condition[_j + 1].Length - 7);
+
+                                if (x.Contains(_compareValue) == false)
+                                {
+                                    _isSatisfy = false;
+                                    break;
+                                }
+                            }
+                            else if (x != Condition[_j + 1])
+                            {
+                                _isSatisfy = false;
+                                break;
+                            }
+                        }
+                    }
+
+                    if (_isSatisfy == false)
+                        continue;
+
+                    foreach(JProperty jProperty in _newValue.Properties())
+                        ListElements[_i][jProperty.Name] = jProperty.Value;
+
+                }
+
+                return;
+            }
+            catch (Exception ex)
+            {
+                DialogResult _errorDialog = MessageBox.Show("Couldn't get or change data\n" + ex, "Error", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+
+                if (_errorDialog == DialogResult.OK)
+                    Application.Exit();
+                return;
+            }
+        }
+        public static JObject ConvertDataRowToJObject(DataRow _dataRow)
+        {
+            JObject _returnObj = new JObject();
+
+            foreach(DataColumn _col in _dataRow.Table.Columns)
+                _returnObj.Add(_col.ColumnName, JToken.FromObject(_dataRow[_col]));
+
+            return _returnObj;
         }
         public JArray Export()
         {
@@ -378,6 +524,27 @@ namespace Calculator
             MessageBox.Show("Export all data!", "Success", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
 
             return _returnData;
+        }
+
+        /// <summary>
+        /// Copy data to another DataProcessing _x
+        /// </summary>
+        /// <param name="_x"></param>
+        public void CopyData(DataProcessing _x)
+        {
+            List<string> _newColumnName = new List<string>();
+            List<Type> _newColumnType = new List<Type>();
+            JArray _newListElements = new JArray();
+
+            foreach (var _p in ShowColumnsName)
+                _newColumnName.Add(_p);
+            foreach (var _p in ShowColumnsType)
+                _newColumnType.Add(_p);
+            foreach (var _p in ListElements)
+                _newListElements.Add(_p);
+
+            _x.Import(_newColumnName, _newColumnType);
+            _x.Import(_newListElements);
         }
     }
 }
